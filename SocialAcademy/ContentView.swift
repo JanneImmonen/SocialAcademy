6e3069ae77f339ded30fbbc3d86b08ccf49ebc4a
Janne <immonenjanne@icloud.com>
2022-03-11 18:37:00 +0200

    Initial Commit

---
+++ SocialAcademy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialAcademy
+//
+//  Created by Janne Immonen on 11.3.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialAcademy/SocialAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAcademyApp.swift
+//  SocialAcademy
+//
+//  Created by Janne Immonen on 11.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
